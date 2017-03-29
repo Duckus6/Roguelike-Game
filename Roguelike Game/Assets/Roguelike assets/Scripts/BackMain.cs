ec56bc067f4d55ce051a606af8a386ab0d2dc51d
Owen Watts <owenrhyswatts@gmail.com>
2017-03-29 21:19:25 +0100

    Extras Back button
    Coded back button and a few QoL changes to Extras to prepare for
    whenever I make the scenes for it

---
+++ Roguelike Game/Assets/Roguelike assets/Scripts/BackMain.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackMain : MonoBehaviour {
+	public void Load () {
+		// Only specifying the sceneName or sceneBuildIndex will load the scene with the Single mode
+		SceneManager.LoadScene (0, LoadSceneMode.Single);
+	}
+}
