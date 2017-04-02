be8fd5e92b85d3a13b1dbed96fadc44815e44112
Owen Watts <owenrhyswatts@gmail.com>
2017-04-02 20:57:34 +0100

    Options Menu
    None of the sliders are connected to audio as no audio is available as
    of yet

---
+++ Roguelike Game/Assets/Roguelike assets/Scripts/OptionsLoadScene.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class OptionsLoadScene : MonoBehaviour {
+	public void Load () {
+		// Only specifying the sceneName or sceneBuildIndex will load the scene with the Single mode
+		SceneManager.LoadScene (2, LoadSceneMode.Single);
+	}
+}
