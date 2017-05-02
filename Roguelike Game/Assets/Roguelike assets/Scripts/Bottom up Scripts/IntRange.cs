4837238b656df5db1a7566298a8e74fb10571004
Owen Watts <owenrhyswatts@gmail.com>
2017-05-02 08:31:40 +0100

    Commenting IntRange
    Just removal of spaces

--- Roguelike Game/Assets/Roguelike assets/Scripts/Bottom up Scripts/IntRange.cs
+++ Roguelike Game/Assets/Roguelike assets/Scripts/Bottom up Scripts/IntRange.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 
-// Serializable so it will show up in the inspector.
+//Serializable so it will show up in the inspector.
 [Serializable]
 public class IntRange
 {
-	public int m_Min;       // The minimum value in this range.
-	public int m_Max;       // The maximum value in this range.
+	public int m_Min;       //The minimum value in this range.
+	public int m_Max;       //The maximum value in this range.
 
 
-	// Constructor to set the values.
+	//Constructor to set the values.
 	public IntRange(int min, int max)
 	{
 		m_Min = min;
@@ -16,7 +16,7 @@ public class IntRange
 	}
 
 
-	// Get a random value from the range.
+	//Get a random value from the range.
 	public int Random
 	{
 		get { return UnityEngine.Random.Range(m_Min, m_Max); }
